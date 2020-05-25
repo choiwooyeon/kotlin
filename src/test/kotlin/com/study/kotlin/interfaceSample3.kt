bc225a8adc709e5fff4c4dd1eb285b9ce6d05573
weoyeon_choi <wooyeon_choi@wemakeprice.com>
2020-05-25 09:42:43 +0900

    kotlin study

--- src/test/kotlin/com/study/kotlin/interfaceSample3.kt
+++ src/test/kotlin/com/study/kotlin/interfaceSample3.kt
@@ -1,4 +1,19 @@
 package com.study.kotlin
 
 class interfaceSample3 {
+
+    interface User {
+        val nickname: String
+    }
+
+    class PrivateUser(override val nickname: String): User
+    class SubscribingUser(val email: String): User {
+        override val nickname: String = getID()
+        fun getID() = email.substringBefore('@')
+    }
+}
+
+fun main(args: Array<String>) {
+    println(interfaceSample3.PrivateUser("Monguse").nickname)
+    println(interfaceSample3.SubscribingUser("monguse@naver.com").nickname)
 }
